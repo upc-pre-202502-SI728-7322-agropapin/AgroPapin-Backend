f9953fc8516ee3cb33d517c6e6973264d096c9eb
Jair Velasquez <106536136+Purak189@users.noreply.github.com>
2025-09-21 20:30:49 -0500

    init: initial commit and basic web project configuration

---
+++ src/main/java/com/agropapin/backend/AgropapinApiApplication.java
@@ -0,0 +1,13 @@
+package com.agropapin.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgropapinApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgropapinApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/agropapin/backend/AgropapinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agropapin.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgropapinApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
