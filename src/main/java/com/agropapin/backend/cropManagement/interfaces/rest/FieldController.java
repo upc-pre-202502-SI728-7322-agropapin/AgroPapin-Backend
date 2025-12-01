2d7d2ad8988650087eabe93d683a597399e30155
Jair Velasquez <106536136+Purak189@users.noreply.github.com>
2025-11-30 22:34:08 -0500

    fix: adding new command for field controler and new facade in organization

--- src/main/java/com/agropapin/backend/cropManagement/interfaces/rest/FieldController.java
+++ src/main/java/com/agropapin/backend/cropManagement/interfaces/rest/FieldController.java
@@ -6,19 +6,20 @@ import com.agropapin.backend.cropManagement.domain.model.commands.UpdateFieldSta
 import com.agropapin.backend.cropManagement.domain.model.queries.GetFieldByFarmerIdQuery;
 import com.agropapin.backend.cropManagement.domain.model.services.FieldCommandService;
 import com.agropapin.backend.cropManagement.domain.model.services.FieldQueryService;
-import com.agropapin.backend.cropManagement.interfaces.rest.resources.CreateFieldResource;
-import com.agropapin.backend.cropManagement.interfaces.rest.resources.FieldResource;
-import com.agropapin.backend.cropManagement.interfaces.rest.resources.UpdateFieldResource;
-import com.agropapin.backend.cropManagement.interfaces.rest.resources.UpdateFieldStatusResource;
+import com.agropapin.backend.cropManagement.interfaces.rest.resources.*;
 import com.agropapin.backend.cropManagement.interfaces.rest.transform.FieldResourceFromEntityAssembler;
 import com.agropapin.backend.iam.interfaces.acl.IamContextFacade;
+import com.agropapin.backend.organizationManagement.interfaces.acl.OrganizationManagementFacade;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -29,11 +30,13 @@ public class FieldController {
     private final FieldQueryService fieldQueryService;
     private final FieldCommandService fieldCommandService;
     private final IamContextFacade iamContextFacade;
+    private final OrganizationManagementFacade organizationManagementFacade;
 
-    public FieldController(FieldQueryService fieldQueryService, FieldCommandService fieldCommandService, IamContextFacade iamContextFacade) {
+    public FieldController(FieldQueryService fieldQueryService, FieldCommandService fieldCommandService, IamContextFacade iamContextFacade, OrganizationManagementFacade organizationManagementFacade) {
         this.fieldQueryService = fieldQueryService;
         this.fieldCommandService = fieldCommandService;
         this.iamContextFacade = iamContextFacade;
+        this.organizationManagementFacade = organizationManagementFacade;
     }
 
     @PostMapping(value = "/me")
@@ -97,6 +100,53 @@ public class FieldController {
         return  ResponseEntity.status(HttpStatus.OK).body(fieldResource);
     }
 
+    @GetMapping(value = "/cooperative/{organizationId}/members")
+    public ResponseEntity<List<MemberFieldResource>> getMembersField(
+            @PathVariable("organizationId") UUID organizationId
+    ){
+
+        var members = organizationManagementFacade.getMembersByCooperativeId(organizationId);
+
+        if (members == null || members.isEmpty()) {
+            return ResponseEntity.ok(Collections.emptyList());
+        }
+
+        var memberFields = members.stream()
+                .map(member -> {
+                    var getFieldCommand = new GetFieldByFarmerIdQuery(member.getUserId());
+
+                    var fieldResourceOptional = fieldQueryService.handle(getFieldCommand);
+
+                    if (fieldResourceOptional.isPresent()) {
+                        var fieldResource = fieldResourceOptional.get();
+
+                        return new MemberFieldResource(
+                                fieldResource.getId(),
+                                fieldResource.getFieldName(),
+                                fieldResource.getLocation(),
+                                fieldResource.getTotalArea(),
+                                fieldResource.getStatus(),
+                                member.getId(),
+                                member.getFullName()
+                        );
+                    } else {
+                        return new MemberFieldResource(
+                                null,
+                                "No Field Assigned",
+                                null,
+                                null,
+                                null,
+                                member.getId(),
+                                member.getFullName()
+                        );
+                    }
+                })
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(memberFields);
+
+    }
+
     @PatchMapping(value = "/{fieldId}/status")
     public ResponseEntity<FieldResource> updateFieldStatus(
             @PathVariable("fieldId") UUID fieldId,
@@ -109,4 +159,7 @@ public class FieldController {
         return updatedField.map(field -> ResponseEntity.ok(FieldResourceFromEntityAssembler.toResourceFromEntity(field)))
                 .orElse(ResponseEntity.notFound().build());
     }
+
+
+
 }
--- src/main/java/com/agropapin/backend/organizationManagement/interfaces/acl/OrganizationManagementFacade.java
+++ src/main/java/com/agropapin/backend/organizationManagement/interfaces/acl/OrganizationManagementFacade.java
@@ -1,17 +1,18 @@
 package com.agropapin.backend.organizationManagement.interfaces.acl;
 
 import com.agropapin.backend.organizationManagement.domain.model.aggregates.Administrator;
+import com.agropapin.backend.organizationManagement.domain.model.aggregates.Cooperative;
 import com.agropapin.backend.organizationManagement.domain.model.aggregates.Farmer;
 import com.agropapin.backend.organizationManagement.domain.model.commands.CreateAdministratorCommand;
 import com.agropapin.backend.organizationManagement.domain.model.commands.CreateFarmerCommand;
 import com.agropapin.backend.organizationManagement.domain.model.queries.GetAdministratorByUserIdAsyncQuery;
+import com.agropapin.backend.organizationManagement.domain.model.queries.GetCooperativeByIdQuery;
 import com.agropapin.backend.organizationManagement.domain.model.queries.GetFarmerByUserIdAsyncQuery;
-import com.agropapin.backend.organizationManagement.domain.services.AdministratorCommandService;
-import com.agropapin.backend.organizationManagement.domain.services.AdministratorQueryService;
-import com.agropapin.backend.organizationManagement.domain.services.FarmerCommandService;
-import com.agropapin.backend.organizationManagement.domain.services.FarmerQueryService;
+import com.agropapin.backend.organizationManagement.domain.model.queries.GetMembersByCooperativeId;
+import com.agropapin.backend.organizationManagement.domain.services.*;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -20,12 +21,14 @@ public class OrganizationManagementFacade {
     private final FarmerCommandService farmerCommandService;
     private final AdministratorQueryService administratorQueryService;
     private final AdministratorCommandService administratorCommandService;
+    private final CooperativeQueryService cooperativeQueryService;
 
-    public OrganizationManagementFacade(FarmerQueryService farmerQueryService, FarmerCommandService farmerCommandService, AdministratorQueryService administratorQueryService, AdministratorCommandService administratorCommandService){
+    public OrganizationManagementFacade(FarmerQueryService farmerQueryService, FarmerCommandService farmerCommandService, AdministratorQueryService administratorQueryService, AdministratorCommandService administratorCommandService, CooperativeQueryService cooperativeQueryService){
         this.farmerQueryService = farmerQueryService;
         this.farmerCommandService = farmerCommandService;
         this.administratorQueryService = administratorQueryService;
         this.administratorCommandService = administratorCommandService;
+        this.cooperativeQueryService = cooperativeQueryService;
     }
 
     public Farmer getFarmerByUserId(String userId){
@@ -48,5 +51,12 @@ public class OrganizationManagementFacade {
     public void createAdministrator(String email, String userId){
         var createAdministratorCommand = new CreateAdministratorCommand(email, userId);
         var administrator = this.administratorCommandService.handle(createAdministratorCommand);
+    }
+
+    public List<Farmer> getMembersByCooperativeId(UUID cooperativeId){
+        var getMembersByCooperativeId = new GetCooperativeByIdQuery(cooperativeId);
+        var cooperative = this.cooperativeQueryService.handle(getMembersByCooperativeId);
+
+        return cooperative.map(Cooperative::getMembers).orElse(null);
     }
 }
